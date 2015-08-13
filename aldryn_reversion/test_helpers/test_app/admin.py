44753a982250baab501d3fff5c7c4f5396584bee
Kirill Kniazev <kirill.kniazev@divio.ch>
2015-08-13 17:50:35 +0300

    Tests helper app: Initail helper app setup. Models and  admin.

---
+++ aldryn_reversion/test_helpers/test_app/admin.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from parler.admin import TranslatableAdmin
+from aldryn_reversion.admin import VersionedPlaceholderAdminMixin
+from cms.admin.placeholderadmin import FrontendEditableAdminMixin
+
+from .models import (
+    SimpleRegistered, WithTranslations, WithPlaceholder, SimpleFK,
+    ComplexOneFK, BlankFK, MultiLevelFK, FKtoSelf,
+)
+
+
+class SimpleRegisteredAdmin(VersionedPlaceholderAdminMixin, admin.ModelAdmin):
+    pass
+
+
+class WithTranslationsAdmin(VersionedPlaceholderAdminMixin,
+                            TranslatableAdmin):
+    pass
+
+
+class WithPlaceholdersAdmin(VersionedPlaceholderAdminMixin,
+                            FrontendEditableAdminMixin,
+                            admin.ModelAdmin):
+    pass
+
+
+class SimpleFKAdmin(VersionedPlaceholderAdminMixin, admin.ModelAdmin):
+    pass
+
+
+class ComplexOneFKAdmin(VersionedPlaceholderAdminMixin,
+                        FrontendEditableAdminMixin,
+                        TranslatableAdmin):
+    pass
+
+
+class BlankFKAdmin(VersionedPlaceholderAdminMixin, admin.ModelAdmin):
+    pass
+
+
+class MultiLevelFKAdmin(VersionedPlaceholderAdminMixin, admin.ModelAdmin):
+    pass
+
+
+class FKtoSelfAdmin(VersionedPlaceholderAdminMixin, admin.ModelAdmin):
+    pass
+
+
+admin.site.register(SimpleRegistered, SimpleRegisteredAdmin)
+admin.site.register(WithTranslations, WithTranslationsAdmin)
+admin.site.register(WithPlaceholder, WithPlaceholdersAdmin)
+admin.site.register(SimpleFK, SimpleFKAdmin)
+admin.site.register(ComplexOneFK, ComplexOneFKAdmin)
+admin.site.register(BlankFK, BlankFKAdmin)
+admin.site.register(MultiLevelFK, MultiLevelFKAdmin)
+admin.site.register(FKtoSelf, FKtoSelfAdmin)
